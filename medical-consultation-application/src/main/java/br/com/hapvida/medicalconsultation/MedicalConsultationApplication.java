0b60403943edf8c60b3a6e65075a3ad7f9903b03
danilotiago <programacao.desenvolvimento@gmail.com>
2021-08-04 12:21:33 -0300

    criando modulos + startup da aplicacao

--- medical-consultation-application/src/main/java/br/com/hapvida/medicalconsultation/MedicalConsultationApplication.java
+++ medical-consultation-application/src/main/java/br/com/hapvida/medicalconsultation/MedicalConsultationApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicalConsultationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalConsultationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalConsultationApplication.class, args);
+    }
 }
