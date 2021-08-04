e0b649bd10fcc1d59b6d182b24441b736eb8f2e4
danilotiago <programacao.desenvolvimento@gmail.com>
2021-08-04 12:09:03 -0300

    initial commit

---
+++ src/test/java/br/com/hapvida/medicalconsultation/MedicalConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hapvida.medicalconsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
